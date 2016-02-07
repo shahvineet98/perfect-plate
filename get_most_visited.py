4423259e90dd00439979ee477e1dca4db3c04400
shahvineet98 <shahvineet98@gmail.com>
2016-02-07 07:45:37 +0000

    added basic yelp requirements in python file

--- get_most_visited.py
+++ get_most_visited.py
@@ -1,6 +1,19 @@
 #Gets the most visited pages with a valid API key
 import requests
 import json
+from requests_oauthlib import OAuth1
+
+zip_code = 20871
+term = ""
+
+
+# Pass term and zip code into url1 below
+url1 = "https://api.yelp.com/v2/search?term=Starbucks&location=20871"
+auth = OAuth1('CiC7-ll3e7KsXa6VBRD97Q','oeYypIyfVM7GrU4qgmoSn0unev4','SC5M3LImmeyhdW_ojNvWtvMhObbLTjkg','xEtFqALhn89vSN4Uh0CxLwBecBE')
+
+yelpr = requests.get(url1.json(), auth=auth)
+
+
 r=requests.get('http://api.reimaginebanking.com/merchants?key=36ad13980d64cfd6d1e2e561c370afa2').json()
 frequencyArray={}
 i=0
